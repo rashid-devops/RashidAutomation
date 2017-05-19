a00a6d90851ac1b8f098fb0c3f3a37daf3d7bf24
rashid-devops <rashid.bhaldar@gmail.com>
2017-05-19 11:11:50 +0530

    Create Newjava.java

---
+++ Newjava.java
@@ -0,0 +1,11 @@
+public class Hellojava 
+{
+public static void main(String[] args)
+{
+for (int i=1;i<=10;i++)
+{
+System.out.println ("Hello Admin your Project run from Git");
+System.out.println("Hello Rashid \t "   +i);
+} 
+}
+}
